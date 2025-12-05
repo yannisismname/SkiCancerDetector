96a9462d96ea07fe3f2abcc5a3a70a50e1901d3d
yannisismname <akosenyannis@gmail.com>
2025-12-05 00:34:58 -0300

    update

--- Backend/main.py
+++ Backend/main.py
@@ -8,7 +8,7 @@ import traceback
 
 app = FastAPI(title="Skin Cancer AI Backend API")
 
-# Configure logging
+# Configure logging``
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
